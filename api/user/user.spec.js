4b04d9acd81f1b92eecc6167a12a02076ca0e83e
ImNM <water0641@naver.com>
2021-04-27 17:48:46 +0900

    upload

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -1,7 +1,7 @@
 
 const request = require('supertest')
 const should = require('should');
-const app = require('./index');
+const app = require('../../index');
 describe('GET /users는 ',()=>{
     describe('성공시', ()=>{
         it('유저 객체를 담은 배열로 응답한',(done)=>{
